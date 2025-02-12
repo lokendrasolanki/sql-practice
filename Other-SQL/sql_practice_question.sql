a3d1e85c316feff1cf6031275b908677e98b98de
lokendrasolanki <lokendrasolanki60@gmail.com>
2025-02-13 03:20:39 +0530

    update

--- Other-SQL/sql_practice_question.sql
+++ Other-SQL/sql_practice_question.sql
@@ -343,4 +343,132 @@ names[2] as secound_name,
 names[3]  as last_name 
 from cte
 
+--- Q1: Delete duplicate data --- 
+
+drop table if exists cars;
+create table cars
+(
+	model_id		int primary key,
+	model_name		varchar(100),
+	color			varchar(100),
+	brand			varchar(100)
+);
+insert into cars values(1,'Leaf', 'Black', 'Nissan');
+insert into cars values(2,'Leaf', 'Black', 'Nissan');
+insert into cars values(3,'Model S', 'Black', 'Tesla');
+insert into cars values(4,'Model X', 'White', 'Tesla');
+insert into cars values(5,'Ioniq 5', 'Black', 'Hyundai');
+insert into cars values(6,'Ioniq 5', 'Black', 'Hyundai');
+insert into cars values(7,'Ioniq 6', 'White', 'Hyundai');
+
+-- SOL-1
+select * from cars where model_id not in (
+select min(model_id) from cars
+group by model_name, brand
+
+);
+
+-- SOL-2
+select * from cars where model_id in (
+select min(model_id) from cars
+group by model_name, brand
+having count(1)>1
+);
+
+-- SOL-3
+select * from cars where model_id in (
+select model_id from (select *,
+Row_number() over(partition by model_name, brand order by model_id desc) rn
+from cars
+) where rn=1
+);
+
+--- Q2: Display highest and lowest salary --- in each department
+
+drop table if exists employee;
+create table employee
+(
+	id 			int primary key GENERATED ALWAYS AS IDENTITY,
+	name 		varchar(100),
+	dept 		varchar(100),
+	salary 		int
+);
+insert into employee values(default, 'Alexander', 'Admin', 6500);
+insert into employee values(default, 'Leo', 'Finance', 7000);
+insert into employee values(default, 'Robin', 'IT', 2000);
+insert into employee values(default, 'Ali', 'IT', 4000);
+insert into employee values(default, 'Maria', 'IT', 6000);
+insert into employee values(default, 'Alice', 'Admin', 5000);
+insert into employee values(default, 'Sebastian', 'HR', 3000);
+insert into employee values(default, 'Emma', 'Finance', 4000);
+insert into employee values(default, 'John', 'HR', 4500);
+insert into employee values(default, 'Kabir', 'IT', 8000);
+
+-- SOL-
+select *, 
+max(salary) over(partition by dept order by salary desc) highest_salary,
+min(salary) over(partition by dept order by salary desc 
+range between unbounded preceding and unbounded following ) lowest_salary 
+from employee;
+
+--- Q3 : Find actual distance --- 
+
+drop table if exists car_travels;
+create table car_travels
+(
+    cars                    varchar(40),
+    days                    varchar(10),
+    cumulative_distance     int
+);
+insert into car_travels values ('Car1', 'Day1', 50);
+insert into car_travels values ('Car1', 'Day2', 100);
+insert into car_travels values ('Car1', 'Day3', 200);
+insert into car_travels values ('Car2', 'Day1', 0);
+insert into car_travels values ('Car3', 'Day1', 0);
+insert into car_travels values ('Car3', 'Day2', 50);
+insert into car_travels values ('Car3', 'Day3', 50);
+insert into car_travels values ('Car3', 'Day4', 100);
+
+with cte as(
+select *, lag(cumulative_distance,1,0) over(partition by cars order by days ) distance from car_travels
+)
+select * , cumulative_distance-distance from cte
+;
+
+--- Q4 : Convert the given input to expected output --- 
+
+drop table src_dest_distance;
+create table src_dest_distance
+(
+    source          varchar(20),
+    destination     varchar(20),
+    distance        int
+);
+insert into src_dest_distance values ('Bangalore', 'Hyderbad', 400);
+insert into src_dest_distance values ('Hyderbad', 'Bangalore', 400);
+insert into src_dest_distance values ('Mumbai', 'Delhi', 400);
+insert into src_dest_distance values ('Delhi', 'Mumbai', 400);
+insert into src_dest_distance values ('Chennai', 'Pune', 400);
+insert into src_dest_distance values ('Pune', 'Chennai', 400);
+
+WITH
+	CTE AS (
+		SELECT
+			*,
+			ROW_NUMBER() OVER () RN
+		FROM
+			SRC_DEST_DISTANCE
+	)
+SELECT
+	S.SOURCE,
+	S.DESTINATION,
+	S.DISTANCE
+FROM
+	CTE S
+	JOIN CTE S1 ON S.RN > S1.RN
+WHERE
+	S.SOURCE = S1.DESTINATION
+	AND S.DESTINATION = S1.SOURCE;
+
+
 
