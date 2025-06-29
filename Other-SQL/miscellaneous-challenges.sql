ad139d3a1a7f8a76561f08084b8f93ea4ff6877a
lokendrasolanki <lokendrasolanki60@gmail.com>
2025-06-29 18:52:14 +0530

    update

--- Other-SQL/miscellaneous-challenges.sql
+++ Other-SQL/miscellaneous-challenges.sql
@@ -169,5 +169,87 @@ on p.purchase_timestamp BETWEEN  c.delivered_at and  c.effective_notification_ti
 group by c.notification_id
 ORDER By c.notification_id;
 
+/*
+In your organization, each employee has a fixed joining salary recorded at the time they start. Over time, employees may receive one or more promotions, each offering a certain percentage increase to their current salary.
+
+ 
+
+You're given two datasets:
+
+employees :  contains each employee’s name and joining salary.
+
+promotions:  lists all promotions that have occurred, including the promotion date and the percent increase granted during that promotion.
+
+Your task is to write a SQL query to compute the current salary of every employee by applying each of their promotions increase round to 2 decimal places.
+If an employee has no promotions, their current salary remains equal to the joining salary. Order the result by emp id.
+*/
+
+CREATE TABLE employees (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    joining_salary INT NOT NULL
+);
+
+INSERT INTO employees (id, name, joining_salary) VALUES
+(1, 'Alice', 50000),
+(2, 'Bob', 60000),
+(3, 'Charlie', 70000),
+(4, 'David', 55000),
+(5, 'Eva', 65000),
+(6, 'Frank', 48000),
+(7, 'Grace', 72000),
+(8, 'Henry', 51000);
+
+-- Create Table Script
+CREATE TABLE employee_promotions (
+    emp_id INT,
+    promotion_date DATE,
+    percent_increase INT
+);
+
+-- Insert Data Script
+INSERT INTO employee_promotions (emp_id, promotion_date, percent_increase) VALUES
+(1, '2021-01-15', 10),
+(1, '2022-03-20', 20),
+(2, '2023-01-01', 5),
+(2, '2024-01-01', 10),
+(3, '2022-05-10', 5),
+(3, '2023-07-01', 10),
+(3, '2024-10-10', 5),
+(4, '2021-09-21', 15),
+(4, '2022-09-25', 15),
+(4, '2023-09-01', 15),
+(4, '2024-09-30', 15),
+(5, '2023-02-01', 10),
+(5, '2023-12-01', 10),
+(6, '2022-06-15', 5),
+(6, '2023-11-11', 10),
+(7, '2022-01-01', 7);
+
+WITH promotion_multipliers AS (
+    SELECT 
+        emp_id,
+		
+        promotion_date,
+        (1 + percent_increase / 100.0) AS multiplier,
+        ROW_NUMBER() OVER (PARTITION BY emp_id ORDER BY promotion_date) AS promotion_order
+    FROM employee_promotions
+),
+cumulative_promotions AS (
+    SELECT 
+        emp_id,
+        EXP(SUM(LN(multiplier))) AS total_multiplier
+    FROM promotion_multipliers
+    GROUP BY emp_id, multiplier
+)
+SELECT 
+    e.id ,
+    e.name,
+    e.joining_salary as initial_salary,
+    -- COALESCE(cp.total_multiplier, 1.0) AS total_multiplier,
+    ROUND(e.joining_salary * COALESCE(cp.total_multiplier, 1.0), 2) AS current_salary
+FROM employees e
+LEFT JOIN cumulative_promotions cp ON e.id = cp.emp_id
+ORDER BY e.id;
 
 
